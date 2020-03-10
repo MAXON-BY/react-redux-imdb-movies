6176d62f404c4adb113af2d733d2c44fa5f96480
MAXON-BY <maxon.pro2015@gmail.com>
2020-03-10 22:14:34 +0300

    Updates

---
+++ precache-manifest.412714e8536ec5992e7726a688000c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4284e8b7211449f00f1aa0124b62f988",
+    "url": "/react-redux-imdb-movies/index.html"
+  },
+  {
+    "revision": "e8cf27d1ada9e65d91ef",
+    "url": "/react-redux-imdb-movies/static/css/main.40b8334c.chunk.css"
+  },
+  {
+    "revision": "8a022a2c1b8faf87e410",
+    "url": "/react-redux-imdb-movies/static/js/2.bb8fd816.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-redux-imdb-movies/static/js/2.bb8fd816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cf27d1ada9e65d91ef",
+    "url": "/react-redux-imdb-movies/static/js/main.1d0d6462.chunk.js"
+  },
+  {
+    "revision": "e22d7201426ec943755a",
+    "url": "/react-redux-imdb-movies/static/js/runtime-main.f370de5f.js"
+  }
+]);
